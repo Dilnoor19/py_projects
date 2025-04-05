e3f9b3be9679e1266a6b7db34a46f9cd277e9dc9
Dilnoor singh <dilnoor3179@gmail.com>
2025-04-05 15:37:51 +0530

    Uploaded via automation script by Mario 🚀📂

---
+++ 01_calculator.py
@@ -0,0 +1,18 @@
+a = int(input("Enter the first no. : "))
+b = int(input("Enter the second no. : "))
+x = input("Enter operation : ")
+
+if x == "+":
+    print("The Solution is : ", a + b)
+if x == "-":
+    print("The Solution is : ", a - b)
+if x == "*":
+    print("The Solution is : ", a * b)
+if x == "/":
+    print("The Solution is : ", a / b)
+if x == "%":
+    print("The Solution is : ", a % b)
+if x == "//":
+    print("The Solution is : ", a // b)
+if x == "**":
+    print("The Solution is : ", a ** b)
